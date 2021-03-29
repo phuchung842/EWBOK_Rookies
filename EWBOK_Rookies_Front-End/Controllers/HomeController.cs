5967f237a280f173e724d654a372e2f4ad7359c9
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-03-29 08:59:27 +0700

    Add project files.

---
+++ EWBOK_Rookies_Front-End/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using EWBOK_Rookies_Front_End.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EWBOK_Rookies_Front_End.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EWBOK_Rookies_Front-End/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EWBOK_Rookies_Front_End.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
