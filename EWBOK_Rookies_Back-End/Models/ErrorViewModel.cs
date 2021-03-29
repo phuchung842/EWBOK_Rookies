df6b1977c8f0ba96f11413befa1d52c2976a8448
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-03-29 11:29:45 +0700

    Add project backend

---
+++ EWBOK_Rookies_Back-End/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EWBOK_Rookies_Back_End.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
